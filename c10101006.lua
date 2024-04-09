daa730cf242ae14bd109fefac642357256535be0
jwyxym <130654339+jwyxym@users.noreply.github.com>
2024-04-09 18:58:20 +0800

    Add files via upload

--- c10101006.lua
+++ c10101006.lua
@@ -4,25 +4,23 @@ function cm.initial_effect(c)
 	vgf.VgCard(c)
 	vgf.AddCodeList(c,10101009)
 	vgd.EffectTypeTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_SINGLE,EVENT_ATTACK_ANNOUNCE,cm.operation2,nil,cm.condition2)
-	vgd.EffectTypeTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_FIELD,EVENT_CUSTOM+m,cm.operation3,vgf.OverlayCost(2),cm.condition3)
 end
 function cm.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	VgF.AtkUp(c,c,10000,nil)
-	Duel.RaiseEvent(c,EVENT_CUSTOM+m,e,0,tp,tp,0)
-end
-function cm.operation3(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_LEAVEONFIELD)
-	local g=Duel.SelectTarget(tp,vgf.RMonsterFilter,tp,0,LOCATION_MZONE,1,1,nil)
-	if g then
-		Duel.HintSelection(g)
-		Duel.SendtoGrave(g,REASON_EFFECT)
+	if Duel.GetMatchingGroup(VgF.VMonsterFilter,tp,LOCATION_MZONE,0,nil,nil):GetFirst():GetOverlayGroup():FilterCount(Card.IsAbleToGraveAsCost,nil)>=2 and Duel.SelectYesNo(vgf.Stringid(m,3)) then
+		local cg=Duel.GetMatchingGroup(VgF.VMonsterFilter,tp,LOCATION_MZONE,0,nil):GetFirst():GetOverlayGroup():FilterSelect(tp,Card.IsAbleToGraveAsCost,2,2,nil)
+        if Duel.SendtoGrave(cg,REASON_COST)==2 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_LEAVEONFIELD)
+			local g=Duel.SelectTarget(tp,vgf.RMonsterFilter,tp,0,LOCATION_MZONE,1,1,nil)
+			if g then
+				Duel.HintSelection(g)
+				Duel.SendtoGrave(g,REASON_EFFECT)
+			end
+		end
 	end
 end
 function cm.condition2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return vgf.RMonsterCondition(e) and c:GetFlagEffectLabel(ConditionFlag)==201 and vgf.VMonsterFilter(Duel.GetAttackTarget())
-end
-function cm.condition3(e,tp,eg,ep,ev,re,r,rp)
-	return eg:GetFirst()==e:GetHandler()
 end
