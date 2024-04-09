5afde1c319b7d80f8df24b100142ad4ffbbc5d44
jwyxym <130654339+jwyxym@users.noreply.github.com>
2024-04-09 19:14:39 +0800

    Add files via upload

--- c10101015.lua
+++ c10101015.lua
@@ -2,7 +2,15 @@
 local cm,m,o=GetID()
 function cm.initial_effect(c)
 	vgf.VgCard(c)
-	vgd.SpellActivate(c,m,vgf.SearchCard(LOCATION_DROP,cm.filter))
+	vgd.SpellActivate(c,m,cm.operation)
+end
+function cm.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKUP)
+	local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.HintSelection(g)
+	VgF.AtkUp(c,g,5000,nil)
+	vgf.SearchCard(LOCATION_DROP,cm.filter)
 end
 function cm.filter(c)
 	return c:IsCode(10101006)
