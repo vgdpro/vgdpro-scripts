b3143e7f52dee54dc4325a5768bf4227adf8b56b
jwyxym <130654339+jwyxym@users.noreply.github.com>
2024-04-11 17:57:42 +0800

    Update c10103009.lua

--- c10103009.lua
+++ c10103009.lua
@@ -1,7 +1,6 @@
 --救命天使 库拉比耶尔
 local cm,m,o=GetID()
 function cm.initial_effect(c)
-    VgF.VgCard(c)
-    vgd.CardTog(c,vgf,DisCardCostg(1))
-    
+    vgf.VgCard(c)
+    vgd.CardToG(c,vgf.DisCardCostg(1))
 end
