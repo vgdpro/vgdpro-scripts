0edc86e46a77805fe0e62394d1af9013de3ab743
jwyxym <130654339+jwyxym@users.noreply.github.com>
2024-04-11 17:51:38 +0800

    Update c10103001.lua

--- c10103001.lua
+++ c10103001.lua
@@ -3,7 +3,8 @@ local cm,m,o=GetID()
 function cm.initial_effect(c)
     VgF.VgCard(c)
     --【自】【V】【1回合1次】：你的攻击判定将等级3的卡判出的战斗结束时，通过【费用】[将手牌中的1张卡舍弃]，选择你的1张后防者，重置，这个回合中，那个单位的力量+10000。
-    vgd.EffectTyperTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_SINGLE,EVENT_DAMAGE_STEP_END,cm.operation,vgf.DisCardCost(1),VMonsterCondition,nil,1,EFFECT_FLAG_DAMAGE_STEP)
+    vgd.EffectTyperTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_SINGLE,EVENT_DAMAGE_STEP_END,cm.operation,vgf.DisCardCost(1),vgf.VMonsterCondition,nil,1,EFFECT_FLAG_DAMAGE_STEP)
+    --【永】【V】：你的回合中，你所有的等级3的单位的力量+2000。
     
 end
 function cm.operation(e,tp,eg,ep,ev,re,r,rp)
@@ -18,5 +19,3 @@ function cm.operation(e,tp,eg,ep,ev,re,r,rp)
 end
 
 end
---有一个永效果没写，效果如下↓
---【永】【V】：你的回合中，你所有的等级3的单位的力量+2000。
