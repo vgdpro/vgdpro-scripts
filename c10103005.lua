335e47fcc43f11a95b9e3f7c3ed48ca456bb6fad
jwyxym <130654339+jwyxym@users.noreply.github.com>
2024-04-11 17:55:03 +0800

    Update c10103005.lua

--- c10103005.lua
+++ c10103005.lua
@@ -3,9 +3,9 @@ local cm,m,o=GetID()
 function cm.initial_effect(c)
     VgF.VgCard(c)
     --【自】【R】：这个单位攻击时，通过【费用】[计数爆发1]，这次战斗中，这个单位的力量+5000。
-    vgd.EffectTypeTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_SINGLE,EVENT_ATTACK_ANNOUNCE,cm.operation,DamageCost(1),RMonsterCondition)
+    vgd.EffectTypeTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_SINGLE,EVENT_ATTACK_ANNOUNCE,cm.operation,vgf.DamageCost(1),vgf.RMonsterCondition)
 end
 function cm.operation(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    VgF.AtkUp(c,c,5000,nil)
+    vgf.AtkUp(c,c,5000,nil)
 end
