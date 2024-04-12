74d436750be7af1a9362bf85c7bbef4b7b9f0e44
jwyxym <130654339+jwyxym@users.noreply.github.com>
2024-04-12 13:32:05 +0800

    Add files via upload

--- c10101006.lua
+++ c10101006.lua
@@ -2,7 +2,7 @@
 local cm,m,o=GetID()
 function cm.initial_effect(c)
 	vgf.VgCard(c)
-	VgD.OverArm(c,10101009)
+	vgd.OverArm(c,10101009)
 	vgd.EffectTypeTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_SINGLE,EVENT_ATTACK_ANNOUNCE,cm.operation2,nil,cm.condition2)
 end
 function cm.operation2(e,tp,eg,ep,ev,re,r,rp)
