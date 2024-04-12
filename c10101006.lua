89d797aeaa202332a613e01e9a8cdf12df999681
jwyxym <130654339+jwyxym@users.noreply.github.com>
2024-04-12 18:31:48 +0800

    Add files via upload

--- c10101006.lua
+++ c10101006.lua
@@ -15,6 +15,7 @@ function cm.operation2(e,tp,eg,ep,ev,re,r,rp)
 			local g=Duel.SelectTarget(tp,vgf.RMonsterFilter,tp,0,LOCATION_MZONE,1,1,nil)
 			if g then
 				Duel.HintSelection(g)
+				
 				Duel.SendtoGrave(g,REASON_EFFECT)
 			end
 		end
