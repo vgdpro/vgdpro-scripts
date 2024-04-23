4c6fb74627bb03cf84f45ab73d8e3f1896dba7e0
Flagsh33p <156975679+Flagsh33p@users.noreply.github.com>
2024-04-23 16:10:57 +0800

    四炎之魔宝龙（除拥宝之龙牙）脚本完成
    拥宝之龙牙不会写QAQ

--- c10202005.lua
+++ c10202005.lua
@@ -1,4 +1,19 @@
+--野蛮侵袭
 local cm,m,o=GetID()
 function cm.initial_effect(c)
 	vgf.VgCard(c)
+	--【自】【R】：这个单位攻击时，你有含有「道拉珠艾尔德」的先导者的话，通过【费用】[计数爆发1]，这次战斗中，这个单位的力量+10000。这次战斗结束时，将这个单位放置到灵魂里。
+	vgd.EffectTypeTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_SINGLE,EVENT_ATTACK_ANNOUNCE,cm.operation,VgF.DamageCost(1),cm.condition)
+end
+function cm.operation(e,tp,eg,ep,ev,re,r,rp)
+	--无法设置重置时点于战斗结束时
+	local c=e:GetHandler()
+	VgF.AtkUp(c,c,10000,EVENT_BATTLED)
+	vgd.EffectTypeTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_SINGLE,EVENT_BATTLED,cm.operation2)
+end
+function cm.condition(e,tp,eg,ep,ev,re,r,rp)
+	return VgF.RMonsterFilter(e:GetHandler()) and Duel.GetAttacker()==e:GetHandler() and VgF.GetVMonster(tp):IsSetCard(0xe8)
+end
+function cm.operation2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Overlay(VgF.GetVMonster(tp),e:GetHandler())
 end
