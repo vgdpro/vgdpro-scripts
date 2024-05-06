51a1432591161a817e76aad8b5e80849a7ada447
jwyxym <130654339+jwyxym@users.noreply.github.com>
2024-05-06 16:16:30 +0800

    Add files via upload

--- c10203001.lua
+++ c10203001.lua
@@ -2,6 +2,7 @@ local cm,m,o=GetID()
 function cm.initial_effect(c)
 	vgf.VgCard(c)
 	--【永】：这张卡将要被RIDE之际，这张卡也当做「朱斯贝克 “破天黎骑”」使用。
+	--【反抗舞装】
 	vgd.EffectTypeTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_SINGLE,EVENT_BATTLED,cm.operation,nil,cm.condition)
 	--【永】【R】：这个回合中曾有你的等级3以上的先导者登场过的话，这个单位的力量+5000。
 	vgd.EffectTypeContinuousChangeAttack(c,EFFECT_TYPE_SINGLE,5000,cm.con)
@@ -28,6 +29,7 @@ function cm.operation(e,tp,eg,ep,ev,re,r,rp)
 end
 function cm.condition(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	return Duel.GetAttacker()==c
 end
 function cm.filter(c,e,tp)
 	return c:IsSetCard(0x76) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK)
