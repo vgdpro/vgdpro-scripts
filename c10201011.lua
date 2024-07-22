0553b8bd4a1728864abf5064d76d0d18efe37d4f
Kirikokora <88840162+Kirikokora@users.noreply.github.com>
2024-07-22 15:55:57 +0800

    Update c10201011.lua

--- c10201011.lua
+++ c10201011.lua
@@ -1,4 +1,15 @@
+-- 【自】：你的RIDE阶段中这张卡被从手牌舍弃时，你可以将这张卡CALL到R上。
 local cm,m,o=GetID()
 function cm.initial_effect(c)
 	vgf.VgCard(c)
+	
+	vgd.EffectTypeTrigger(c,m,LOCATION_DROP,EFFECT_TYPE_SINGLE,EVENT_DISCARD,cm.operation,nil,cm.condition)
+end
+function cm.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()==PHASE_STANDBY
+end
+function cm.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	-- local rc=vgf.GetMatchingGroup(vgf.VMonsterFilter,tp,LOCATION_MZONE,0,nil):GetFirst()
+	vgf.Call(c,0,tp)
 end
