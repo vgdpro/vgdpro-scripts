095ba8c9731ba0e54ae954096ea96ffc29f63280
jwyxym <jwyxym@126.com>
2024-09-19 21:34:08 +0800

    Update c10101001.lua

--- c10101001.lua
+++ c10101001.lua
@@ -2,8 +2,8 @@
 local cm,m,o=GetID()
 function cm.initial_effect(c)
 	vgf.VgCard(c)
-	vgd.EffectTypeIgnition(c,m,LOCATION_MZONE,vgf.SearchCard(LOCATION_MZONE,LOCATION_DROP,cm.filter),vgf.DisCardCost(1),nil,nil,1)
-	vgd.EffectTypeTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_SINGLE,EVENT_ATTACK_ANNOUNCE,cm.operation,vgf.DamageCost(1))
+	vgd.EffectTypeIgnition(c,m,LOCATION_MZONE,vgf.SearchCard(LOCATION_MZONE,LOCATION_DROP,cm.filter),vgf.DisCardCost(1),vgf.VMonsterCondition,nil,1)
+	vgd.EffectTypeTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_SINGLE,EVENT_ATTACK_ANNOUNCE,cm.operation,vgf.DamageCost(1),vgf.VMonsterCondition)
 end
 function cm.filter(c)
 	return c:IsLevel(0)
@@ -15,10 +15,4 @@ function cm.operation(e,tp,eg,ep,ev,re,r,rp)
 	local sg=vgf.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0x201)
 	if sg:GetCount()>0 then g:Merge(sg) end
 	vgf.AtkUp(c,g,10000)
-end
-function cm.op(e,tp,eg,ep,ev,re,r,rp)
-	local g=vgf.GetMatchingGroup(nil,tp,LOCATION_DAMAGE,0,nil)
-	for tc in vgf.Next(g) do
-		Duel.ChangePosition(tc,POS_FACEDOWN)
-	end
 end
