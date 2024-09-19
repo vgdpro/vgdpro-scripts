d834622488e460336efdf3b7cc2443864379ffe7
jwyxym <jwyxym@126.com>
2024-09-19 20:26:49 +0800

    Update c10101001.lua

--- c10101001.lua
+++ c10101001.lua
@@ -2,7 +2,7 @@
 local cm,m,o=GetID()
 function cm.initial_effect(c)
 	vgf.VgCard(c)
-	vgd.EffectTypeIgnition(c,m,LOCATION_MZONE,vgf.SearchCard(LOCATION_HAND,LOCATION_MZONE,LOCATION_DROP,cm.filter),vgf.DisCardCost(1),nil,nil,1)
+	vgd.EffectTypeIgnition(c,m,LOCATION_MZONE,vgf.SearchCard(LOCATION_MZONE,LOCATION_DROP,cm.filter),vgf.DisCardCost(1),nil,nil,1)
 	vgd.EffectTypeTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_SINGLE,EVENT_ATTACK_ANNOUNCE,cm.operation,vgf.DamageCost(1))
 end
 function cm.filter(c)
@@ -11,7 +11,7 @@ end
 function cm.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Group.CreateGroup()
-	if c:IsRelateToEffect(e) then g:AddCard(c) end
+	if c:IsRelateToEffect(e) and c:IsFaceup() then g:AddCard(c) end
 	local sg=vgf.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0x201)
 	if sg:GetCount()>0 then g:Merge(sg) end
 	vgf.AtkUp(c,g,10000)
