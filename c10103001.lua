122419b9b82f0f4693893e6ed059b0b68a2223df
jwyxym <jwyxym@126.com>
2024-09-19 22:10:56 +0800

    Update c10103001.lua

--- c10103001.lua
+++ c10103001.lua
@@ -4,7 +4,7 @@ function cm.initial_effect(c)
     vgf.VgCard(c)
     --【自】【V】【1回合1次】：你的攻击判定将等级3的卡判出的战斗结束时，通过【费用】[将手牌中的1张卡舍弃]，选择你的1张后防者，重置，这个回合中，那个单位的力量+10000。
     vgd.EffectTypeTrigger(c,m,LOCATION_MZONE,EFFECT_TYPE_FIELD,EVENT_BATTLED,cm.operation,vgf.DisCardCost(1),cm.condition,nil,1)
-    vgd.GlobalCheckEffect(c,m,EVENT_MOVE,cm.checkcon)
+    vgd.GlobalCheckEffect(c,m,EVENT_MOVE,cm.checkcon,cm.checkop)
     --【永】【V】：你的回合中，你所有的等级3的单位的力量+2000。
     vgd.EffectTypeContinuousChangeAttack(c,m,EFFECT_TYPE_FIELD,2000,cm.con,cm.target,LOCATION_MZONE+LOCATION_GZONE,0)
 end
@@ -35,4 +35,8 @@ end
 function cm.con(e)
     local tp=e:GetHandlerPlayer()
     return vgf.VMonsterCondition(e) and Duel.GetTurnPlayer()==tp
+end
+function cm.checkop(e,tp,eg,ep,ev,re,r,rp)
+    local e1=Duel.RegisterFlagEffect(tp,m,RESET_PHASE+PHASE_END,0,1)
+    vgf.EffectReset(e:GetHandler(),e1,EVENT_BATTLED)
 end
