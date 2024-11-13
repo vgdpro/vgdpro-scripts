4e4b23a661d8e8ff229597fd021aae413a516f8a
whenmo <91626218+whenmo@users.noreply.github.com>
2024-11-13 12:55:34 +0800

    fix CannotBeTarget

--- c10402023.lua
+++ c10402023.lua
@@ -1,7 +1,7 @@
 local cm,m,o=GetID()
 function cm.initial_effect(c)
 	vgf.VgCard(c)
-	vgd.CannotBeTarget(c,m,vgf.RMonsterCondition,nil,LOCATION_MZONE,cm.tg)
+	vgd.CannotBeTarget(c,m,LOCATION_RZONE,EFFECT_TYPE_FIELD,nil,nil,cm.tg)
 end
 function cm.tg(e,tc)
 	local c=e:GetHandler()
